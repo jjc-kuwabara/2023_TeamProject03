2fa5ee5d06e1643d8a2f6af3f63a2580fb571419
NoguchiEikichi <aymek.5aikichi@icloud.com>
2023-03-30 14:50:32 +0900

    DebugManagerに使えそうな処理を追加

--- Assets/Scripts/Manager/DebugManager.cs
+++ Assets/Scripts/Manager/DebugManager.cs
@@ -4,16 +4,44 @@ using UnityEngine;
 
 public class DebugManager : MonoBehaviour
 {
+    public GameObject scroll;
+    ScrollManager scroll_M;
+
     void Start()
     {
-        
+        scroll_M = scroll.GetComponent<ScrollManager>();
     }
 
     void Update()
     {
+        Debug_Quick();
+
+        Debug_Air();
+
         Debug_Eat();
 
         Debug_Reset();
+    }
+
+    void Debug_Quick()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            scroll_M.moveSpeed *= 2;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            scroll_M.moveSpeed /= 2;
+        }
+    }
+
+    void Debug_Air()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GameManager.Instance.AirGet();
+        }
     }
 
     void Debug_Eat()
