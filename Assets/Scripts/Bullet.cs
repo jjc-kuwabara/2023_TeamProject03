860c46e367b805000150dc31acb4afe96d1f9121
NoguchiEikichi <aymek.5aikichi@icloud.com>
2023-04-11 13:37:24 +0900

    バレットが消える条件の修正

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -31,7 +31,7 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.transform.tag != "Eat" && collision.transform.tag != "Player")
+        if(collision.transform.tag != "Eat" && collision.transform.tag != "Player" && collision.transform.tag != "PlayerAttack")
         {
             //Instantiate(effectExp, transform.position, transform.rotation);
 
