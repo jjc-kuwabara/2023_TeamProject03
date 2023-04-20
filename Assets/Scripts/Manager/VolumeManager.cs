edf20f86108807655fd75cb9d8c100f8a2840df9
NoguchiEikichi <aymek.5aikichi@icloud.com>
2023-04-20 11:50:54 +0900

    音量調整用の処理の追加

---
+++ Assets/Scripts/Manager/VolumeManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeManager : MonoBehaviour
+{
+    public Slider bgmSlider;
+    public Slider seSlider;
+
+    float bgmVolCurrent;
+    float seVolCurrent;
+
+    void Start()
+    {
+        bgmSlider.value = PlayerPrefs.GetInt("Vol_BG", 8);
+        seSlider.value = PlayerPrefs.GetInt("Vol_SE", 8);
+
+        bgmVolCurrent = bgmSlider.value;
+        seVolCurrent = seSlider.value;
+    }
+
+    public void BGMBolumeChange()
+    {
+        bgmVolCurrent = bgmSlider.value;
+
+        SoundManager.Instance.VolumeChange((int)bgmVolCurrent, (int)seVolCurrent);
+    }
+
+    public void SEBolumeChange()
+    {
+        seVolCurrent = seSlider.value;
+
+        SoundManager.Instance.VolumeChange((int)bgmVolCurrent, (int)seVolCurrent);
+    }
+}
